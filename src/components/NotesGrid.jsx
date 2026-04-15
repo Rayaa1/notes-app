c323cafd4765c029cf876573e17c1c08cd3d4a52
Rayaa1 <raya.koleva@abv.bg>
2026-04-15 18:35:31 +0300

    Fix onEdit correctly from NotesGrid to NoteCard

--- src/components/NotesGrid.jsx
+++ src/components/NotesGrid.jsx
@@ -1,10 +1,15 @@
 import NoteCard from "./NoteCard";
 
-function NotesGrid({ notes, onDelete }) {
+function NotesGrid({ notes, onDelete, onEdit }) {
   return (
     <div className="notes-grid">
       {notes.map(note => (
-        <NoteCard key={note.id} note={note} onDelete={onDelete} />
+        <NoteCard
+          key={note.id}
+          note={note}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
       ))}
     </div>
   );
